60cae805a1edf2ceb697625fd6a460c6b74d9892
masudimubashir@gmail.com <masudimubashir@gmail.com>
2024-05-21 14:58:36 +0530

    new commit

--- resources/views/Front/ItSolution/it-solutions.blade.php
+++ resources/views/Front/ItSolution/it-solutions.blade.php
@@ -137,7 +137,7 @@
 
         <section class="o-section is--mt-265 md--mt-200 sm--mt-150" style="padding-top: 5rem">
             <div class="o-container is--z-3 md--vflex">
-            
+
                 <div class="o-l503 is--ml-84 md--ml-52 sm--auto">
                     <h2 class="o-h-72">
                         Web Development <br />
@@ -229,13 +229,7 @@
 
         <section class="o-section is--mt-265 md--mt-200 sm--mt-150">
             <div class="o-container is--z-3 md--vflex">
-                <div data-w-id="d00272e5-a7a2-d740-6265-132f7be6c6a4" class="c-bpg">
-
-                    <figure style="opacity:0" class="c-bpg__img">
-                        <img src="" loading="lazy" alt="" class="o-img" />
-                    </figure>
-
-                </div>
+                
                 <div class="o-l503 is--ml-84 md--ml-52 sm--auto">
                     <h2 class="o-h-72">
                         APP DEVELOPMENT <br />
