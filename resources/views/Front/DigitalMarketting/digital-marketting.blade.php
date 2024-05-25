415e32a3252b6f49a01e012a1eee314feb182976
masudimubashir@gmail.com <masudimubashir@gmail.com>
2024-05-25 09:57:23 +0530

    14:46

--- resources/views/Front/DigitalMarketting/digital-marketting.blade.php
+++ resources/views/Front/DigitalMarketting/digital-marketting.blade.php
@@ -35,7 +35,7 @@
             }
         </style>
     </div>
-   
+
     <div style="opacity:0" class="o-page">
         @include('Front.Navbar')
         <header class="o-section s--mt-n56">
@@ -105,7 +105,7 @@
                             Lead Generation
 
                             <div class="learn_more">
-                                <a href="{{route('lead-generation')}}">Learn More</a>
+                                <a href="{{ route('lead-generation') }}">Learn More</a>
                             </div>
                         </h1>
                         <div class="o-layout is--mt-66">
@@ -154,8 +154,7 @@
                                             <path id="Oval" fill-rule="evenodd" clip-rule="evenodd"
                                                 d="M8.97825 9.68276L9.70083 8.96018C10.1826 8.47846 10.1826 7.69743 9.70083 7.21571L8.97825 6.49313C8.49652 6.01141 7.7155 6.01141 7.23378 6.49313L6.51119 7.21571C6.02947 7.69743 6.02947 8.47846 6.51119 8.96018L7.23378 9.68276C7.7155 10.1645 8.49652 10.1645 8.97825 9.68276Z"
                                                 fill="white" />
-                                            <path id="Shape" opacity="0.4" fill-rule="evenodd"
-                                                clip-rule="evenodd"
+                                            <path id="Shape" opacity="0.4" fill-rule="evenodd" clip-rule="evenodd"
                                                 d="M8.72272 1.29812L8.72258 0H7.48906L7.48915 1.23927C7.48919 1.7838 7.12951 2.26044 6.62625 2.4684C6.15774 2.662 5.61314 2.93204 5.30679 3.23839L3.25631 5.28887L3.07086 5.48783C2.85961 5.73113 2.66178 6.12902 2.50766 6.49442C2.29764 6.99235 1.82503 7.34718 1.28462 7.34724L0 7.34739V8.58092L1.28755 8.5813C1.82378 8.58146 2.2948 8.93062 2.50749 9.42287C2.70017 9.86883 2.96444 10.3834 3.25631 10.6753L5.30679 12.7258L5.51693 12.9209C5.77899 13.1467 6.21188 13.3507 6.60545 13.506C7.11922 13.7087 7.48919 14.189 7.48915 14.7413L7.48906 16H8.72258L8.72272 14.6496C8.72277 14.1247 9.05768 13.6611 9.53479 13.4421C9.94909 13.252 10.4217 12.9973 10.6932 12.7258L12.7437 10.6753L12.9319 10.4731C13.1474 10.2244 13.3472 9.81644 13.5018 9.44292C13.7097 8.94055 14.1844 8.58146 14.7281 8.5813L16 8.58092V7.34739L14.7001 7.34724C14.167 7.34718 13.698 7.00214 13.4835 6.51418C13.2914 6.07733 13.0297 5.57488 12.7437 5.28887L10.6932 3.23839L10.4777 3.03862C10.2533 2.84602 9.89245 2.66085 9.55618 2.51304C9.06832 2.29858 8.72277 1.83104 8.72272 1.29812ZM5.90977 3.51898C7.16375 2.43031 9.06473 2.48215 10.2571 3.67451L12.3076 5.72499L12.4631 5.89185C13.5518 7.14583 13.4999 9.04681 12.3076 10.2392L10.2571 12.2896L10.0902 12.4452C8.83625 13.5338 6.93527 13.482 5.74291 12.2896L3.69243 10.2392L3.53691 10.0723C2.44823 8.81833 2.50007 6.91735 3.69243 5.72499L5.74291 3.67451L5.90977 3.51898Z"
                                                 fill="white" />
                                         </g>
@@ -330,13 +329,28 @@
                     <div class="o-l1452 m--l527 s--100">
                         <h1 class="o-h-112 m--57 gradient">Appointments Setting
                             <div class="learn_more">
-                                <a href="{{route('appointment-settings')}}">Learn More</a>
+                                <a href="{{ route('appointment-settings') }}">Learn More</a>
                             </div>
+
                         </h1>
+                        <div class="o-layout is--mt-66">
+                            <p class="o-txt-20--med">
+                                <span>
+
+                                    Booking sales qualified appointments for a company like ‍Companies like Yours
+                                    involves a
+                                    strategic approach that combines digital marketing, relationship-building, and
+                                    effective
+                                    communication.
+                                </span>
+                            </p>
+
+                        </div>
 
 
                         <div data-w-id="03b1306c-723f-f3cb-4434-c432e347caee"
                             class="o-flex-ss is--mt-123--pl34--ml-n40 m--mt72--pl-56--obo s--p-0--mt-56rem">
+
                             <div class="o-l525 s--auto">
                                 <div style="-webkit-transform:translate3d(0, 100%, 0) scale3d(1, 1, 1) rotateX(0) rotateY(0) rotateZ(0) skew(0, 0);-moz-transform:translate3d(0, 100%, 0) scale3d(1, 1, 1) rotateX(0) rotateY(0) rotateZ(0) skew(0, 0);-ms-transform:translate3d(0, 100%, 0) scale3d(1, 1, 1) rotateX(0) rotateY(0) rotateZ(0) skew(0, 0);transform:translate3d(0, 100%, 0) scale3d(1, 1, 1) rotateX(0) rotateY(0) rotateZ(0) skew(0, 0);opacity:0"
                                     class="o-flex-ss u-anim-1">
@@ -531,14 +545,14 @@
                 <div class="o-flex-ss is--ml-80--pt-144 m--ml-57--pt-164--obo s--pt-0">
                     <div class="o-l522 m--l484 s--100">
                         <h1 class="o-h-112 m--57 gradient">Social Media Marketing <div class="learn_more">
-                                <a href="{{route('social-media-marketing')}}">Learn More</a>
+                                <a href="{{ route('social-media-marketing') }}">Learn More</a>
                             </div>
                         </h1>
 
                     </div>
                     <div data-w-id="03b1306c-723f-f3cb-4434-c432e347caee"
                         class="o-flex-ss is--mt-123--pl34--ml-n40 m--mt72--pl-56--obo s--p-0--mt-56rem">
-                        <div class="o-l525 s--auto">
+                        <div class="o-l525 s--auto" style="padding-left: 0px !important;">
                             <div style="-webkit-transform:translate3d(0, 100%, 0) scale3d(1, 1, 1) rotateX(0) rotateY(0) rotateZ(0) skew(0, 0);-moz-transform:translate3d(0, 100%, 0) scale3d(1, 1, 1) rotateX(0) rotateY(0) rotateZ(0) skew(0, 0);-ms-transform:translate3d(0, 100%, 0) scale3d(1, 1, 1) rotateX(0) rotateY(0) rotateZ(0) skew(0, 0);transform:translate3d(0, 100%, 0) scale3d(1, 1, 1) rotateX(0) rotateY(0) rotateZ(0) skew(0, 0);opacity:0"
                                 class="o-flex-ss u-anim-1">
                                 <figure class="o-ico is--mr-32 m--mr-20">
@@ -712,7 +726,7 @@
                 <div class="o-layout is--mt-312--ml-72 m--ml-57--mt-0 s--ml-0--mt-0">
                     <div class="o-l1130 m--l567 s--100">
                         <h1 class="o-h-112 m--57 gradient">Email Marketing <div class="learn_more">
-                                <a href="{{route('email-marketing')}}">Learn More</a>
+                                <a href="{{ route('email-marketing') }}">Learn More</a>
                             </div>
                         </h1>
 
