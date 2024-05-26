7fcc3a75f29a9970213ebc9a448e04b54fca0534
masudimubashir@gmail.com <masudimubashir@gmail.com>
2024-05-26 10:49:35 +0530

    ..

--- resources/views/Front/Footer.blade.php
+++ resources/views/Front/Footer.blade.php
@@ -2,9 +2,9 @@
 <footer class="o-section is--pt-48--pb-40--z-5 m--pt-24--pb-0">
     <figure class="c-footer-bg"></figure>
     <div class="o-container is--z-3--pl-78 m--auto--centered">
-        <div class="o-flex-strsbw is--mr-240 md--centered s--vflex" style="font:bolder;">
-                 <div class="o-txt-20 gradient" >© Sourch Technologies Pvt. Ltd. 2024</div>
-                <a href="{{route('privacy_policy')}}" class="c-foter-link is--gutter">Privacy Policy</a>
+        <div class="o-flex-strsbw is--mr-240 md--centered s--vflex" >
+                 <div class="o-txt-20 gradient" style="font:initial !important;">© Sourch Technologies Pvt. Ltd. 2024</div>
+                <a href="{{route('privacy_policy')}}" style="font:initial !important;" class="c-foter-link is--gutter">Privacy Policy</a>
         </div>
     </div>
 </footer>
